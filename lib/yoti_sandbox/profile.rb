44dd3a3c99e0ed01a3a10b85ee867926437212dd
David Grayston <davidgrayston@googlemail.com>
2020-03-24 22:17:52 +0000

    SDK-1470: Implement AgeVerificationBuilder

--- lib/yoti_sandbox/profile.rb
+++ lib/yoti_sandbox/profile.rb
@@ -1,4 +1,5 @@
+require_relative 'profile/age_verification'
 require_relative 'profile/anchor'
 require_relative 'profile/attribute'
-require_relative 'profile/token_request'
 require_relative 'profile/client'
+require_relative 'profile/token_request'
--- lib/yoti_sandbox/profile/token_request.rb
+++ lib/yoti_sandbox/profile/token_request.rb
@@ -38,143 +38,167 @@ module Yoti
           self
         end
 
-        def with_attribute(
+        def with_attribute(attribute)
+          @attributes.push attribute
+          self
+        end
+
+        def with_given_names(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::GIVEN_NAMES,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_family_name(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::FAMILY_NAME,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_full_name(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::FULL_NAME,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_date_of_birth(date, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::DATE_OF_BIRTH,
+              value: date.strftime('%F'),
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_age_verification(age_verification)
+          with_attribute(age_verification.to_attribute)
+        end
+
+        def with_gender(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::GENDER,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_phone_number(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::PHONE_NUMBER,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_nationality(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::NATIONALITY,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_postal_address(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::POSTAL_ADDRESS,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_structured_postal_address(data, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::STRUCTURED_POSTAL_ADDRESS,
+              value: data.to_json,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_selfie(data, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::SELFIE,
+              value: Base64.strict_encode64(data),
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_email_address(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::EMAIL_ADDRESS,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def with_document_details(value, optional: false, anchors: [])
+          with_attribute(
+            create_attribute(
+              name: Yoti::Attribute::DOCUMENT_DETAILS,
+              value: value,
+              optional: optional,
+              anchors: anchors
+            )
+          )
+        end
+
+        def build
+          TokenRequest.new(@remember_me_id, @attributes)
+        end
+
+        private
+
+        def create_attribute(
           name:,
           value:,
           derivation: '',
           optional: false,
           anchors: []
         )
-          @attributes.push Yoti::Sandbox::Profile::Attribute.new(
+          Yoti::Sandbox::Profile::Attribute.new(
             name: name,
             value: value,
             derivation: derivation,
             optional: optional,
             anchors: anchors
           )
-          self
-        end
-
-        def with_given_names(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::GIVEN_NAMES,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_family_name(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::FAMILY_NAME,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_full_name(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::FULL_NAME,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_date_of_birth(date, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::DATE_OF_BIRTH,
-            value: date.strftime('%F'),
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_age_verification(dob:, derivation:, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::DATE_OF_BIRTH,
-            value: dob.strftime('%F'),
-            derivation: derivation,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_gender(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::GENDER,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_phone_number(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::PHONE_NUMBER,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_nationality(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::NATIONALITY,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_postal_address(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::POSTAL_ADDRESS,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_structured_postal_address(data, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::STRUCTURED_POSTAL_ADDRESS,
-            value: data.to_json,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_selfie(data, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::SELFIE,
-            value: Base64.strict_encode64(data),
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_email_address(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::EMAIL_ADDRESS,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def with_document_details(value, optional: false, anchors: [])
-          with_attribute(
-            name: Yoti::Attribute::DOCUMENT_DETAILS,
-            value: value,
-            optional: optional,
-            anchors: anchors
-          )
-        end
-
-        def build
-          TokenRequest.new(@remember_me_id, @attributes)
         end
       end
     end
