fcd385f5c3f71a77e8b6a3fe4288307fd7735359
David Grayston <davidgrayston@googlemail.com>
2020-03-24 18:23:23 +0000

    SDK-1470: Rename Sandbox to Yoti::Sandbox

--- lib/yoti_sandbox/profile/client.rb
+++ lib/yoti_sandbox/profile/client.rb
@@ -3,39 +3,41 @@
 require 'yoti'
 require 'securerandom'
 
-module Sandbox
-  module Profile
-    class Client
-      def initialize(base_url: nil)
-        @base_url = base_url || "#{Yoti.configuration.api_url}/sandbox/v1"
-      end
-
-      def setup_sharing_profile(token_request)
-        endpoint = "/apps/#{Yoti.configuration.client_sdk_id}/tokens?\
-nonce=#{SecureRandom.uuid}&timestamp=#{Time.now.to_i}"
-        uri = URI(
-          "#{@base_url}#{endpoint}"
-        )
-
-        response = Net::HTTP.start(
-          uri.hostname,
-          uri.port,
-          use_ssl: true
-        ) do |http|
-          unsigned = Net::HTTP::Post.new uri
-          unsigned.body = token_request.to_json
-          signed_request = Yoti::SignedRequest.new(
-            unsigned,
-            endpoint,
-            token_request
-          ).sign
-          http.request signed_request
+module Yoti
+  module Sandbox
+    module Profile
+      class Client
+        def initialize(base_url: nil)
+          @base_url = base_url || "#{Yoti.configuration.api_url}/sandbox/v1"
         end
 
-        raise "Failed to share profile #{response.code}: #{response.body}" unless response.code == '201'
+        def setup_sharing_profile(token_request)
+          endpoint = "/apps/#{Yoti.configuration.client_sdk_id}/tokens?\
+nonce=#{SecureRandom.uuid}&timestamp=#{Time.now.to_i}"
+          uri = URI(
+            "#{@base_url}#{endpoint}"
+          )
 
-        token = JSON.parse(response.body)['token']
-        token
+          response = Net::HTTP.start(
+            uri.hostname,
+            uri.port,
+            use_ssl: true
+          ) do |http|
+            unsigned = Net::HTTP::Post.new uri
+            unsigned.body = token_request.to_json
+            signed_request = Yoti::SignedRequest.new(
+              unsigned,
+              endpoint,
+              token_request
+            ).sign
+            http.request signed_request
+          end
+
+          raise "Failed to share profile #{response.code}: #{response.body}" unless response.code == '201'
+
+          token = JSON.parse(response.body)['token']
+          token
+        end
       end
     end
   end
