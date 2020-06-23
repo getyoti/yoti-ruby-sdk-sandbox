61313283c9733edf19307313a846d7188d296334
David Grayston <davidgrayston@googlemail.com>
2020-06-23 17:10:46 +0100

    SDK-1625: Format sandbox API error message

--- lib/yoti_sandbox/doc_scan/client.rb
+++ lib/yoti_sandbox/doc_scan/client.rb
@@ -19,29 +19,39 @@ module Yoti
         # @param [Yoti::SandboxDocScan::Request::ResponseConfig] response_config
         #
         def configure_session_response(session_id, response_config)
-          Yoti::Request
-            .builder
-            .with_http_method('PUT')
-            .with_base_url(@base_url)
-            .with_endpoint("sessions/#{session_id}/response-config")
-            .with_query_param('sdkId', Yoti.configuration.client_sdk_id)
-            .with_payload(response_config)
-            .build
-            .execute
+          request = Yoti::Request
+                    .builder
+                    .with_http_method('PUT')
+                    .with_base_url(@base_url)
+                    .with_endpoint("sessions/#{session_id}/response-config")
+                    .with_query_param('sdkId', Yoti.configuration.client_sdk_id)
+                    .with_payload(response_config)
+                    .build
+
+          begin
+            request.execute
+          rescue Yoti::RequestError => e
+            raise Yoti::Sandbox::DocScan::Error.wrap(e)
+          end
         end
 
         #
         # @param [Yoti::SandboxDocScan::Request::ResponseConfig] response_config
         #
         def configure_application_response(response_config)
-          Yoti::Request
-            .builder
-            .with_http_method('PUT')
-            .with_base_url(@base_url)
-            .with_endpoint("apps/#{Yoti.configuration.client_sdk_id}/response-config")
-            .with_payload(response_config)
-            .build
-            .execute
+          request = Yoti::Request
+                    .builder
+                    .with_http_method('PUT')
+                    .with_base_url(@base_url)
+                    .with_endpoint("apps/#{Yoti.configuration.client_sdk_id}/response-config")
+                    .with_payload(response_config)
+                    .build
+
+          begin
+            request.execute
+          rescue Yoti::RequestError => e
+            raise Yoti::Sandbox::DocScan::Error.wrap(e)
+          end
         end
       end
     end
