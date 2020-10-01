ebaf5706fc20c10137217c46aafaec49c0c335cf
David Grayston <davidgrayston@googlemail.com>
2020-10-01 11:51:06 +0100

    SDK-1759: Add support for document ID photo

--- lib/yoti_sandbox/doc_scan/request/task/document_text_data_extraction_task.rb
+++ lib/yoti_sandbox/doc_scan/request/task/document_text_data_extraction_task.rb
@@ -44,13 +44,17 @@ module Yoti
         class DocumentTextDataExtractionTaskResult
           #
           # @param [Hash,nil] document_fields
+          # @param [DocumentIdPhoto,nil] document_id_photo
           #
-          def initialize(document_fields)
+          def initialize(document_fields, document_id_photo = nil)
             unless document_fields.nil?
               Validation.assert_is_a(Hash, document_fields, 'document_fields')
               document_fields.each { |_k, v| Validation.assert_respond_to(:to_json, v, 'document_fields value') }
             end
             @document_fields = document_fields
+
+            Validation.assert_is_a(DocumentIdPhoto, document_id_photo, 'document_id_photo', true)
+            @document_id_photo = document_id_photo
           end
 
           def to_json(*_args)
@@ -59,7 +63,8 @@ module Yoti
 
           def as_json(*_args)
             {
-              document_fields: @document_fields
+              document_fields: @document_fields,
+              document_id_photo: @document_id_photo
             }.compact
           end
         end
@@ -101,10 +106,21 @@ module Yoti
           end
 
           #
+          # @param [String] content_type
+          # @param [bin] data
+          #
+          # @return [self]
+          #
+          def with_document_id_photo(content_type, data)
+            @document_id_photo = DocumentIdPhoto.new(content_type, data)
+            self
+          end
+
+          #
           # @return [DocumentTextDataExtractionTask]
           #
           def build
-            result = DocumentTextDataExtractionTaskResult.new(@document_fields)
+            result = DocumentTextDataExtractionTaskResult.new(@document_fields, @document_id_photo)
             DocumentTextDataExtractionTask.new(result, @document_filter)
           end
         end
